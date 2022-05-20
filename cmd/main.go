02bdbe56e6dac81925a3d4f25a0331cc502bf369
Bofrostmann07 <84816381+Bofrostmann07@users.noreply.github.com>
2022-05-20 16:05:45 +0200

    feat: menu to choose tool

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,47 @@
 package main
 
-import "network_go/internal/eth_int_search"
+import (
+	"fmt"
+	"network_go/internal/eth_int_search"
+	"network_go/internal/inventory"
+	"network_go/internal/maclookup"
+)
 
 func main() {
-	appConfig := eth_int_search.AppConfig{
-		Username: "automate",
-		Password: "automateme",
-		SSHPort:  "22",
+	fmt.Println("Started network_go\n")
+	menu()
+}
+
+func menu() {
+	for true {
+		appConfig := eth_int_search.AppConfig{
+			Username: "automate",
+			Password: "automateme",
+			SSHPort:  "22",
+		}
+		fmt.Println("Please choose the Tool by number:\n" +
+			"1 - Interface search\n" +
+			"2 - MAC address batch lookup\n" +
+			"99 - Show Config Values (global_config.yml)",
+		)
+
+		var toolNumber int
+		fmt.Print("Tool number: ")
+		_, err := fmt.Scan(&toolNumber)
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		switch toolNumber {
+		case 1:
+			switchInventory := inventory.ReadSwitchInventoryFromCSV()
+			eth_int_search.FetchEthIntConfig(&switchInventory, appConfig)
+		case 2:
+			maclookup.DoLookUp()
+		case 99:
+			println("N/A")
+		default:
+			println("Invalid tool number.")
+		}
 	}
-	networkSwitchList := []eth_int_search.NetworkSwitch{{Address: "10.20.0.1", Hostname: "test1"}, {Address: "10.20.0.1", Hostname: "test2"}}
-	eth_int_search.FetchEthIntConfig(&networkSwitchList, appConfig)
-	//maclookup.DoLookUp()
 }
