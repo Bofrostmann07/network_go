32568327a35f0eb344496cc2eb6de2c4f3d7ff3a
Bofrostmann07 <84816381+Bofrostmann07@users.noreply.github.com>
2022-05-21 16:19:40 +0200

    feat: first try to multithread preConnection check

--- internal/ethIntSearch/preConnection.go
+++ internal/ethIntSearch/preConnection.go
@@ -9,37 +9,54 @@ import (
 )
 
 func CheckConnection(switchInventory *[]NetworkSwitch) {
-	checkConnectivity(switchInventory)
+	jobs := make(chan NetworkSwitch, len(*switchInventory))
+	results := make(chan NetworkSwitch, len(*switchInventory))
+
+	for i := 0; i < config.AppConfig.SSH.MaxGoRoutines; i++ {
+		go worker(jobs, results)
+	}
+	for _, networkSwitch := range *switchInventory {
+		jobs <- networkSwitch
+	}
+	//close(jobs)
+
+	for i := 0; i < len(*switchInventory); i++ {
+		(*switchInventory)[i] = <-results
+		fmt.Println(<-results)
+
+	}
 }
 
-func checkConnectivity(switchInventory *[]NetworkSwitch) {
-	errorCounter := 0
-	for i, switchElement := range *switchInventory {
-		timeout := time.Second
-		conn, err := net.DialTimeout("tcp", net.JoinHostPort(switchElement.Address, config.AppConfig.SSH.Port), timeout)
-		if err != nil {
-			errorCounter++
-			log.Println("Connecting error:", err)
-		}
-		if conn != nil {
-			defer conn.Close()
-			(*switchInventory)[i].Reachable = true
-		}
+func worker(jobs <-chan NetworkSwitch, results chan<- NetworkSwitch) {
+	for networkSwitch := range jobs {
+		results <- checkConnectivity(networkSwitch)
 	}
-	if errorCounter != 0 {
-		log.Printf("Couldn't reach %d switches. All unreachable switches will be left out.", errorCounter)
+}
 
-		var userInput string
-		fmt.Print("Continue? [y]/n: ")
-		//_, err := fmt.Scanf("\n%s", &userInput) //TODO unexpected newline
-		//if err != nil {
-		//	fmt.Print(err)
-		//}
-		fmt.Scanln(&userInput)
-		if userInput == "\n" || userInput == "y" {
-			log.Println("Continuing with the connection check")
-			return
-		}
-		log.Fatalln("Stopping. Restart the tool after fixing the connection issues.")
+func checkConnectivity(networkSwitch NetworkSwitch) NetworkSwitch {
+	timeout := time.Duration(config.AppConfig.SSH.Timeout) * time.Second
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(networkSwitch.Address, config.AppConfig.SSH.Port), timeout)
+	if err != nil {
+		log.Println("Connecting error:", err)
 	}
+	if conn != nil {
+		defer conn.Close()
+		networkSwitch.Reachable = true
+	}
+	return networkSwitch
+	//if errorCounter != 0 {
+	//	log.Printf("Couldn't reach %d switches. All unreachable switches will be left out.", errorCounter)
+	//
+	//	var userInput string
+	//	fmt.Print("Continue? [y]/n: ")
+	//	//_, err := fmt.Scanf("\n%s", &userInput) //TODO unexpected newline
+	//	//if err != nil {
+	//	//	fmt.Print(err)
+	//	//}
+	//	fmt.Scanln(&userInput)
+	//	if userInput == "\n" || userInput == "y" {
+	//		log.Println("Continuing with the connection check")
+	//		return
+	//	}
+	//	log.Fatalln("Stopping. Restart the tool after fixing the connection issues.")
 }
