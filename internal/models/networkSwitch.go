1a31570d0926a2895477b4eb3da5b393c9dd1849
Bofrostmann07 <84816381+Bofrostmann07@users.noreply.github.com>
2022-05-29 23:19:47 +0200

    feat: search methods for networkSwitch

--- internal/models/networkSwitch.go
+++ internal/models/networkSwitch.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+	"network_go/internal/parser/ast"
+	"strconv"
+)
+
 type NetworkSwitch struct {
 	Address   string `json:"Address"`
 	Hostname  string `json:"Hostname"`
@@ -13,4 +20,76 @@ type NetworkSwitch struct {
 type EthInterface struct {
 	InterfaceConfig []string `json:"InterfaceConfig"`
 	MacList         []string `json:"MacList"`
+}
+
+func (e EthInterface) Search(field ast.Field) bool {
+	switch field.Bucket {
+	case "interface":
+		return field.SearchStringSlice(e.InterfaceConfig)
+	case "mac":
+		return field.SearchStringSlice(e.MacList)
+	}
+	return false
+}
+
+func (n NetworkSwitch) Search(field ast.Field) bool {
+	var searchString string
+
+	switch field.Bucket {
+	case "address":
+		searchString = n.Address
+	case "hostname":
+		searchString = n.Hostname
+	case "platform":
+		searchString = n.Platform
+	case "group":
+		searchString = n.Group
+	case "reachable":
+		searchString = strconv.FormatBool(n.Reachable)
+	case "interface", "mac":
+		newEthInterfaces := make(map[string]EthInterface)
+
+		for s, ethInterface := range n.EthInterfaces {
+			if ethInterface.Search(field) {
+				newEthInterfaces[s] = ethInterface
+			}
+		}
+		if len(newEthInterfaces) == 0 {
+			return false
+		}
+
+		//n.EthInterfaces = newEthInterfaces
+		return true
+	}
+
+	return field.SearchString(searchString)
+}
+
+func (n NetworkSwitch) EvaluateQuery(query *ast.Query) (matches bool) {
+	matches = n.Search(*query.Field)
+
+	// TODO handle nil query
+
+	// No additional queries - Field only
+	if query.AndQuery == nil && query.OrQuery == nil {
+		return matches
+	}
+
+	// Both Queries
+	if query.AndQuery != nil && query.OrQuery != nil {
+		return (matches && n.EvaluateQuery(query.AndQuery)) || n.EvaluateQuery(query.OrQuery)
+	}
+
+	// And Query
+	if query.AndQuery != nil {
+		return matches && n.EvaluateQuery(query.AndQuery)
+	}
+
+	// Or Query
+	if query.OrQuery != nil {
+		return matches || n.EvaluateQuery(query.OrQuery)
+	}
+
+	fmt.Printf("Uncaught state - %v\n", query)
+	return false
 }
